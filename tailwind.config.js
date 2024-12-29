c1eb011e82b81b167b5fd63ea30fa2c83f3dbba9
reemo9 <152188351+amgen1@users.noreply.github.com>
2024-12-29 04:35:09 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        genos: ['"Genos"', 'sans-serif'],
+        montserrat: ['"Montserrat"', 'sans-serif'],
+        poppins: ['"Poppins"', 'sans-serif'],
+        raleway: ['"Raleway"', 'sans-serif'],
+        roboto: ['"Roboto"', 'sans-serif'],
+        tektur: ['"Tektur"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "text": "#FFFFFF",
+          "secondary_text": "#808080",
+          "accent": "#0097E9",
+          "bg": "#000000",
+          "base-100": "#ffffff",
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+    ],
+  },
+}
